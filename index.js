43a2febbd02c9bd6535df52b262404160b59f92d
Brainerd <cbrainerd@deloitte.com>
2016-08-16 07:41:47 -0400

    added 'smooth' scrolling when you expand a tab or accordion

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
   $(document).ready(function(){
     $('ul.tabs').tabs();
+
+    $(".collapsible-header, .tab").click(function() {
+    	// hack hack hack hack
+		var scrolLDown = setInterval(function() {
+			$('html,body').animate({scrollTop: $(document).height()}, 1);
+		}, 1)
+		setTimeout(function() {
+			clearInterval(scrollDown)
+		}, 200)
+
+    })
   });
+
